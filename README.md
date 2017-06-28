07c93608f2390b284364be8e76ce1ae74f4a582d
Brian Salcedo <salcbrian@gmail.com>
2017-06-27 20:47:35 -0400

    simplify - just ASN filtering

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # ASNfilter
-ASNfilter is a DNS proxy (with web interface) for ad blocking and enhanced privacy. Queries are matched against an amalgamated hosts list, and responses are checked against an IP blacklist and ASN whitelist/blacklist.
-
-ASNfilter makes use of GeoLite data created by MaxMind, available from [http://www.maxmind.com](http://www.maxmind.com)
+ASNfilter is a DNS proxy that checks responses against an ASN blacklist. It makes use of GeoLite data created by MaxMind, available from [http://www.maxmind.com](http://www.maxmind.com)
 
 # Documentation
 WIP
--- dnsproxy.py
+++ dnsproxy.py
@@ -8,8 +8,8 @@ import socket
 import geoip2.database
 import redis
 
-from twisted.internet import reactor, defer
-from twisted.names import client, dns, error, server
+from twisted.internet import reactor
+from twisted.names import client, dns, server
 from twisted.names.dns import A, AAAA
 
 
@@ -48,65 +48,18 @@ def responseFilter(ip_address):
     global r
 
     try:
-        if r.sismember('ASNfilter/ip_blacklist', ip_address):
-            return True
-
         asn = get_ASN(ip_address)
 
         r.set('ASNfilter/ASN/' + str(asn.autonomous_system_number),
               asn.autonomous_system_organization)
-        r.sadd('ASNfilter/list', str(asn.autonomous_system_number))
 
-        mode = r.get('ASNfilter/mode')
-
-        if mode is None:
-            r.set('ASNfilter/mode', b'learning')
-            mode = b'learning'
-
-        if mode == b'blacklist':
-            if r.sismember('ASNfilter/asn_blacklist',
-                           str(asn.autonomous_system_number)):
-                return True
-
-        if mode == b'whitelist':
-            if r.sismember('ASNfilter/asn_whitelist',
-                           str(asn.autonomous_system_number)):
-                return True
+        if r.sismember('ASNfilter/blacklist',
+                       str(asn.autonomous_system_number)):
+            return True
     except:
         raise
 
     return False
-
-
-def queriesFilter(queries):
-    global r
-
-    for query in queries:
-        if r.sismember('ASNfilter/hosts_blacklist', query.name.name.lower()):
-            return True
-
-    return False
-
-
-class ASNFilterResolver(client.Resolver):
-    def __init__(self, resolv=None, servers=None, timeout=(1, 3, 11, 45),
-                 reactor=None):
-        self._queryUDP = client.Resolver.queryUDP
-        self._queryTCP = client.Resolver.queryTCP
-
-        client.Resolver.__init__(self, resolv, servers, timeout, reactor)
-
-    def queryUDP(self, queries, timeout=None):
-        if queriesFilter(queries):
-            return defer.fail(error.DomainError())
-        else:
-            return self._queryUDP(self, queries, timeout)
-
-    def queryTCP(self, queries, timeout=10):
-        if queriesFilter(queries):
-            return defer.fail(error.DomainError())
-        else:
-            return self._queryTCP(self, queries, timeout)
 
 
 class ASNFilterDNSServerFactory(server.DNSServerFactory):
@@ -148,26 +101,26 @@ def main():
     config = configparser.ConfigParser()
     config.read('asnfilter.conf')
 
-    pool = redis.ConnectionPool.from_url(config.get('asnfilter', 'redisurl'))
+    pool = redis.ConnectionPool.from_url(config.get('dnsproxy', 'redisurl'))
     r = redis.StrictRedis(connection_pool=pool)
 
-    reader = geoip2.database.Reader(config.get('asnfilter', 'asndb'))
+    reader = geoip2.database.Reader(config.get('dnsproxy', 'asndb'))
 
     servers = []
-    for entry in config.get('asnfilter', 'servers').split(','):
+    for entry in config.get('dnsproxy', 'servers').split(','):
         servers.append((entry.replace(' ', ''), 53))
 
     factory = ASNFilterDNSServerFactory(
-        clients=[ASNFilterResolver(servers=servers)]
+        clients=[client.Resolver(servers=servers)]
     )
 
     protocol = dns.DNSDatagramProtocol(controller=factory)
 
-    reactor.listenUDP(int(config.get('asnfilter', 'port')), protocol)
-    reactor.listenTCP(int(config.get('asnfilter', 'port')), factory)
+    reactor.listenUDP(int(config.get('dnsproxy', 'port')), protocol)
+    reactor.listenTCP(int(config.get('dnsproxy', 'port')), factory)
 
-    drop_privileges(config.get('asnfilter', 'user'),
-                    config.get('asnfilter', 'group'))
+    drop_privileges(config.get('dnsproxy', 'user'),
+                    config.get('dnsproxy', 'group'))
 
     reactor.run()
 
